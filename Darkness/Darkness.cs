c4c837d98284e84a7c38dcbf1d8ff6b63f7516f2
5FiftySix6 <ybham6@gmail.com>
2020-05-27 18:38:54 -0400

    Toggles

--- Darkness/Darkness.cs
+++ Darkness/Darkness.cs
@@ -1,55 +1,119 @@
+using System.Collections.Generic;
 using HutongGames.PlayMaker;
 using Modding;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Darkness : Mod
+namespace Darkness
 {
-        private static string version = "0.0.1";
+    public class Darkness : Mod, ITogglableMod
+    {
+        private const string VERSION = "0.0.2";
 
-        public override string GetVersion()
-        {
-                return version;
-        }
+        private readonly Dictionary<(string Name, string EventName), string> OriginalTransitions = new Dictionary<(string Name, string EventName), string>();
+
+        private bool _enabled = true;
+
+        public override string GetVersion() => VERSION;
 
         public override void Initialize()
         {
-                Log("Removing Light");
-                UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-                Log("Removed Light");
+            if (!_enabled)
+                Darken();
+
+            _enabled = true;
+
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoad;
+            ModHooks.Instance.HeroUpdateHook += Update;
         }
 
-        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        public void Unload()
         {
-                Log("Updating hc.vignetteFSM");
+            if (_enabled)
+                Lighten();
 
-                HeroController instance = HeroController.instance;
+            _enabled = false;
 
-                FsmState[] fsmStates = instance.vignetteFSM.FsmStates;
-
-                foreach (FsmState fsmState in fsmStates)
-                {
-                        FsmTransition[] transitions = fsmState.Transitions;
-
-                        foreach (FsmTransition fsmTransition in transitions)
-                        {
-                                switch (fsmTransition.ToState)
-                                {
-                                        case "Dark -1":
-                                        case "Normal":
-                                        case "Dark 1":
-                                        case "Lantern":
-                                                fsmTransition.ToState = "Dark 2";
-                                                break;
-                                        case "Dark -1 2":
-                                        case "Normal 2":
-                                        case "Dark 1 2":
-                                        case "Lantern 2":
-                                                fsmTransition.ToState = "Dark 2 2";
-                                                break;
-                                }
-
-                                Log(fsmTransition.ToState);
-                        }
-                }
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoad;
+            ModHooks.Instance.HeroUpdateHook -= Update;
         }
+
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.Alpha0)) return;
+
+            if (_enabled)
+                Lighten();
+            else
+                Darken();
+
+            _enabled = !_enabled;
+        }
+
+        private void Lighten()
+        {
+            if (HeroController.instance == null) return;
+
+            foreach (FsmState state in HeroController.instance.vignetteFSM.FsmStates)
+            {
+                foreach (FsmTransition trans in state.Transitions)
+                {
+                    if (!OriginalTransitions.TryGetValue((state.Name, trans.EventName), out string orig)) continue;
+
+                    trans.ToState = orig;
+                }
+            }
+
+            HeroController.instance.vignetteFSM.SetState("Normal");
+
+            HeroController.instance.vignette.enabled = false;
+        }
+
+        private void OnSceneLoad(Scene arg0, LoadSceneMode arg1)
+        {
+            if (HeroController.instance == null) return;
+
+            if (!_enabled) return;
+
+            Log("Updating hc.vignetteFSM");
+
+            Darken();
+        }
+
+        private void Darken()
+        {
+            if (HeroController.instance == null) return;
+
+            foreach (FsmState state in HeroController.instance.vignetteFSM.FsmStates)
+            {
+                foreach (FsmTransition trans in state.Transitions)
+                {
+                    switch (trans.ToState)
+                    {
+                        case "Dark -1":
+                        case "Normal":
+                        case "Dark 1":
+                        case "Lantern":
+
+                            OriginalTransitions[(state.Name, trans.EventName)] = trans.ToState;
+
+                            trans.ToState = "Dark 2";
+                            break;
+                        case "Dark -1 2":
+                        case "Normal 2":
+                        case "Dark 1 2":
+                        case "Lantern 2":
+
+                            OriginalTransitions[(state.Name, trans.EventName)] = trans.ToState;
+
+                            trans.ToState = "Dark 2 2";
+                            break;
+                    }
+                }
+            }
+
+            HeroController.instance.vignetteFSM.SetState("Dark 2");
+            HeroController.instance.vignette.enabled = true;
+        }
+    }
 }
